89aef9df00e54ab298f35aced68165c00f04e622
danindu-dev <hedtperera@gmail.com>
2020-06-20 19:58:17 +0530

    Debug Test

--- EDIX/urls.py
+++ EDIX/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('BPMS/', include('BPMS.urls')),
-    path('bpms/', include('BPMS.urls'))
+    path('bpms/', include('BPMS.urls')),
 ]
 
